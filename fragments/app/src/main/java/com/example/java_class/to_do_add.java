c9e70bbbfdbfcb9e912ea86d0ffc06a39fa74b05
MarvinFhAachen <56964320+MarvinFhAachen@users.noreply.github.com>
2019-12-16 21:59:18 +0100

    to_do_addbuton verbunden
    + butten brngt einen jetzt zur anlegen seite und der abbrechen button wieder zurück

--- fragments/app/src/main/java/com/example/java_class/to_do_add.java
+++ fragments/app/src/main/java/com/example/java_class/to_do_add.java
@@ -4,18 +4,38 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class to_do_add extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do_add, container, false);
+    View v = inflater.inflate(R.layout.fragment_to_do_add, container, false);
+
+        //Onklick listener
+        Button abbrechen = (Button) v.findViewById(R.id.to_do_add_abbrechen);
+        abbrechen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //to_do.to_to_onclick_titel(v , position);
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.nav_host_fragment,new to_do());
+                fr.commit();
+            }
+        });
+
+
+        return v;
     }
 
 }
