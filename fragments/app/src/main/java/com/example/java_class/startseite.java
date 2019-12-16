e6ad9816f829363de8eefb047af37ab333b97ab1
JonahHeisterkamp <58516823+JonahHeisterkamp@users.noreply.github.com>
2019-12-16 23:05:53 +0100

    unfertige Implementierung der Tipps auf der Startseite

--- fragments/app/src/main/java/com/example/java_class/startseite.java
+++ fragments/app/src/main/java/com/example/java_class/startseite.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.Random;
+import java.lang.String;
+import android.view.View;
+import android.widget.TextView;
 
 
 
@@ -19,5 +23,18 @@ public class startseite extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_startseite, container, false);
     }
+
+    // Tests:
+
+    //TextView testlol = (TextView) findViewById(R.id.TippsStart);
+    //testlol.setText("klappt!")
+
+    //String[] array = getResources().getStringArray(R.array.TippsArray);
+    //String randomTipp = array[new Random().nextInt(array.length)];
+
+    //String[] myArrayOfStrings = {"one", "two", "three" };
+    //Random r = new Random();
+    //String myRandString = r.nextInt(myArrayOfStrings.length);
+
 
 }
