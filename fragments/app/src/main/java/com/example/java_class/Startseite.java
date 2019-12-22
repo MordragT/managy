2154e4de6f14a4b59140f8cad17ecd4265f5fa92
JonahHeisterkamp <58516823+JonahHeisterkamp@users.noreply.github.com>
2019-12-22 18:44:36 +0100

    Teil zur Realisierung der Startseite
    Ein paar unfertige Funktionen für die Startseite.

--- fragments/app/src/main/java/com/example/java_class/Startseite.java
+++ fragments/app/src/main/java/com/example/java_class/Startseite.java
@@ -1,5 +1,6 @@
 package com.example.java_class;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,32 +9,67 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 import java.lang.String;
+import java.lang.reflect.Array;
 
 import android.view.View;
 import android.widget.TextView;
 
 
+
 public class Startseite extends Fragment {
+
+    //TextView FristStart = getActivity().findViewById(R.id.FristStart);
+    //TextView TerminStart = getActivity().findViewById(R.id.TerminStart);
+    //TextView ToDoStart = getActivity().findViewById(R.id.ToDoStart);
+    //TextView LiteraturStart = getActivity().findViewById(R.id.LiteraturStart);
+    //TextView TippsStart = getActivity().findViewById(R.id.TippsStart);
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //TippsStartseite();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_startseite, container, false);
     }
 
-    // Tests:
+    public void FristStartseite() {
+        TextView FristStart = getActivity().findViewById(R.id.FristStart);
+    }
 
-    //TextView testlol = (TextView) findViewById(R.id.TippsStart);
-    //testlol.setText("klappt!")
+    public void TerminStartseite() {
+        TextView TerminStart = getActivity().findViewById(R.id.TerminStart);
+        TerminStart.setText(Schnittstelle.terminListe.size());
+    }
 
-    //String[] array = getResources().getStringArray(R.array.TippsArray);
-    //String randomTipp = array[new Random().nextInt(array.length)];
+    public void ToDoStartseite() {
+        TextView ToDoStart = getActivity().findViewById(R.id.ToDoStart);
+        ToDoStart.setText(Schnittstelle.toDoListe.size());
+    }
 
-    //String[] myArrayOfStrings = {"one", "two", "three" };
-    //Random r = new Random();
-    //String myRandString = r.nextInt(myArrayOfStrings.length);
+    public void LiteraturStartseite(){
+        TextView LiteraturStart = getActivity().findViewById(R.id.LiteraturStart);
+        LiteraturStart.setText(Schnittstelle.literaturListe.size());
+    }
+
+    public void TippsStartseite() {
+        TextView TippsStart = getActivity().findViewById(R.id.TippsStart);
+        //TippsStart.setText("klappt");
+        //TippsStart.setText(getResources().getString(R.string.TippStart));
+
+        int randomIndex = new Random().nextInt((getResources().getStringArray(R.array.TippsArray)).length);
+        String randomTipp = (getResources().getStringArray(R.array.TippsArray))[randomIndex];
+        TippsStart.setText(randomTipp);
+
+        //Random r = new Random();
+        //String myRandString = r.nextInt(R.array.TippsArray.);
+
+    }
+
+
 }
